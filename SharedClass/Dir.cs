9d36c4c8f262c4d73981ec20c35fb971b6ad9b83
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-10 01:48:29 +0700

    Create SharedClass project, then add Class FileDir, Dir. The purpose is make dll file to share with Client and Server

---
+++ SharedClass/Dir.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedClass
+{
+    [Serializable]
+    public class Dir
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public List<Dir> SubDirectories { get; set; }
+        public List<FileDir> SubFiles { get; set; }
+
+        public Dir(string name, string path)
+        {
+            this.Name = name;
+            this.Path = path;
+            SubFiles = new List<FileDir>();
+            SubDirectories = new List<Dir>();
+        }
+
+        public Dir() { }
+
+    }
+}
---
+++ SharedClass/FileDir.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedClass
+{
+    [Serializable]
+    public class FileDir
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        public FileDir(string name, string path)
+        {
+            this.Name = name;
+            this.Path = path;
+        }
+
+        public FileDir() { }
+    }
+}
