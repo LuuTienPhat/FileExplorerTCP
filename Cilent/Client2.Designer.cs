f8afaade6fc223b14d030facc27077645c5ee51b
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-09 21:38:57 +0700

    Add: form Client, set as startup

---
+++ Cilent/Client2.Designer.cs
@@ -0,0 +1,60 @@
+﻿
+namespace Cilent
+{
+    partial class Client2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.treeList1 = new DevExpress.XtraTreeList.TreeList();
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // treeList1
+            // 
+            this.treeList1.Location = new System.Drawing.Point(145, 145);
+            this.treeList1.Name = "treeList1";
+            this.treeList1.Size = new System.Drawing.Size(400, 200);
+            this.treeList1.TabIndex = 0;
+            // 
+            // Client2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(757, 425);
+            this.Controls.Add(this.treeList1);
+            this.Name = "Client2";
+            this.Text = "Client2";
+            ((System.ComponentModel.ISupportInitialize)(this.treeList1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraTreeList.TreeList treeList1;
+    }
+}
