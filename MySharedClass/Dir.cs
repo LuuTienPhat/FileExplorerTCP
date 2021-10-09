188d3a0e27e437236da64bc9693536c7e78e1eec
Lưu Tiến Phát <luutienphat@gmail.com>
2021-10-10 05:44:38 +0700

    Create MyShareClass share dll file

---
+++ MySharedClass/Dir.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MySharedClass
+{
+    [Serializable]
+    public partial class Dir
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public List<Dir> SubDirectories { get; set; }
+        public List<FileDir> SubFiles { get; set; }
+
+        public Dir(string name, string path)
+        {
+            Name = name;
+            Path = path;
+            SubFiles = new List<FileDir>();
+            SubDirectories = new List<Dir>();
+        }
+
+        public Dir() { }
+
+    }
+
+    [Serializable]
+    public partial class FileDir
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+
+        public FileDir(string name, string path)
+        {
+            Name = name;
+            Path = path;
+        }
+
+        public FileDir() { }
+    }
+}
