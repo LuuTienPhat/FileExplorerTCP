e94a2e11d532602b1696e9c69bc7896a77216af7
Lưu Tiến Phát <luutienphat@gmail.com>
2021-11-05 23:56:08 +0700

    Change Class members

---
+++ SharedClass/DirectoryView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace SharedClass
+{
+    [Serializable]
+    public partial class DirectoryView
+    {
+        public DirectoryInfo directoryInfo { get; set; }
+        public List<DirectoryView> subDirectories { get; set; }
+        public List<FileView> subFiles { get; set; }
+        public DirectoryView(DirectoryInfo directoryInfo)
+        {
+            this.directoryInfo = directoryInfo;
+            this.subDirectories = new List<DirectoryView>();
+            this.subFiles = new List<FileView>();
+        }
+
+        public DirectoryView(string path)
+        {
+            this.directoryInfo = new DirectoryInfo(path);
+            this.subDirectories = new List<DirectoryView>();
+            this.subFiles = new List<FileView>();
+        }
+
+        public DirectoryView() { }
+
+    }
+}
---
+++ SharedClass/FileView.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SharedClass
+{
+    [Serializable]
+    public partial class FileView
+    {
+        public FileInfo fileInfo { get; set; }
+        public string data { get; set; }
+        public FileView(FileInfo fileInfo, string data)
+        {
+            this.fileInfo = fileInfo;
+            this.data = data;
+        }
+        public FileView(string path, string data)
+        {
+            this.fileInfo = new FileInfo(path);
+            this.data = data;
+        }
+        public FileView() { }
+    }
+}
