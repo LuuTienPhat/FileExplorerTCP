97cc6e525a1e0e19436b44620ddd249e30e9843a
Lưu Tiến Phát <luutienphat@gmail.com>
2021-11-05 23:56:35 +0700

    Add Form Directory Info

---
+++ Cilent/FormDirectoryInfo.Designer.cs
@@ -0,0 +1,282 @@
+﻿
+namespace Cilent
+{
+    partial class FormDirectoryInfo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+
+        private void InitializeComponent()
+        {
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl6 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl8 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl7 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl4 = new DevExpress.XtraEditors.LabelControl();
+            this.txtName = new DevExpress.XtraEditors.TextEdit();
+            this.txtSize = new DevExpress.XtraEditors.TextEdit();
+            this.txtPath = new DevExpress.XtraEditors.TextEdit();
+            this.txtDateModified = new DevExpress.XtraEditors.TextEdit();
+            this.txtDateCreated = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.checkEdit1 = new DevExpress.XtraEditors.CheckEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtName.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSize.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPath.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDateModified.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDateCreated.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.checkEdit1);
+            this.panelControl1.Controls.Add(this.labelControl3);
+            this.panelControl1.Controls.Add(this.textEdit1);
+            this.panelControl1.Controls.Add(this.labelControl2);
+            this.panelControl1.Controls.Add(this.txtDateCreated);
+            this.panelControl1.Controls.Add(this.txtDateModified);
+            this.panelControl1.Controls.Add(this.txtPath);
+            this.panelControl1.Controls.Add(this.txtSize);
+            this.panelControl1.Controls.Add(this.txtName);
+            this.panelControl1.Controls.Add(this.labelControl4);
+            this.panelControl1.Controls.Add(this.labelControl1);
+            this.panelControl1.Controls.Add(this.labelControl7);
+            this.panelControl1.Controls.Add(this.labelControl8);
+            this.panelControl1.Controls.Add(this.labelControl5);
+            this.panelControl1.Controls.Add(this.labelControl6);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(418, 402);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Appearance.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelControl1.Appearance.Options.UseFont = true;
+            this.labelControl1.Location = new System.Drawing.Point(141, 26);
+            this.labelControl1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(143, 24);
+            this.labelControl1.TabIndex = 0;
+            this.labelControl1.Text = "Directory Info";
+            this.labelControl1.Click += new System.EventHandler(this.labelControl1_Click);
+            // 
+            // labelControl6
+            // 
+            this.labelControl6.Location = new System.Drawing.Point(33, 211);
+            this.labelControl6.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.labelControl6.Name = "labelControl6";
+            this.labelControl6.Size = new System.Drawing.Size(80, 16);
+            this.labelControl6.TabIndex = 5;
+            this.labelControl6.Text = "Date Created:";
+            this.labelControl6.Click += new System.EventHandler(this.labelControl6_Click);
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Location = new System.Drawing.Point(33, 169);
+            this.labelControl5.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.labelControl5.Name = "labelControl5";
+            this.labelControl5.Size = new System.Drawing.Size(30, 16);
+            this.labelControl5.TabIndex = 4;
+            this.labelControl5.Text = "Path:";
+            // 
+            // labelControl8
+            // 
+            this.labelControl8.Location = new System.Drawing.Point(33, 126);
+            this.labelControl8.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.labelControl8.Name = "labelControl8";
+            this.labelControl8.Size = new System.Drawing.Size(29, 16);
+            this.labelControl8.TabIndex = 7;
+            this.labelControl8.Text = "Size:";
+            // 
+            // labelControl7
+            // 
+            this.labelControl7.Location = new System.Drawing.Point(33, 254);
+            this.labelControl7.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.labelControl7.Name = "labelControl7";
+            this.labelControl7.Size = new System.Drawing.Size(83, 16);
+            this.labelControl7.TabIndex = 6;
+            this.labelControl7.Text = "Date Modified:";
+            this.labelControl7.Click += new System.EventHandler(this.labelControl7_Click);
+            // 
+            // labelControl4
+            // 
+            this.labelControl4.Location = new System.Drawing.Point(33, 85);
+            this.labelControl4.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.labelControl4.Name = "labelControl4";
+            this.labelControl4.Size = new System.Drawing.Size(38, 16);
+            this.labelControl4.TabIndex = 3;
+            this.labelControl4.Text = "Name:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(141, 82);
+            this.txtName.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.txtName.Name = "txtName";
+            this.txtName.Properties.Appearance.BackColor = System.Drawing.Color.White;
+            this.txtName.Properties.Appearance.Options.UseBackColor = true;
+            this.txtName.Properties.ReadOnly = true;
+            this.txtName.Size = new System.Drawing.Size(240, 22);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtSize
+            // 
+            this.txtSize.Location = new System.Drawing.Point(141, 123);
+            this.txtSize.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.txtSize.Name = "txtSize";
+            this.txtSize.Properties.Appearance.BackColor = System.Drawing.Color.White;
+            this.txtSize.Properties.Appearance.Options.UseBackColor = true;
+            this.txtSize.Properties.ReadOnly = true;
+            this.txtSize.Size = new System.Drawing.Size(240, 22);
+            this.txtSize.TabIndex = 9;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(141, 165);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Properties.Appearance.BackColor = System.Drawing.Color.White;
+            this.txtPath.Properties.Appearance.Options.UseBackColor = true;
+            this.txtPath.Properties.ReadOnly = true;
+            this.txtPath.Size = new System.Drawing.Size(240, 22);
+            this.txtPath.TabIndex = 10;
+            // 
+            // txtDateModified
+            // 
+            this.txtDateModified.Location = new System.Drawing.Point(141, 249);
+            this.txtDateModified.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.txtDateModified.Name = "txtDateModified";
+            this.txtDateModified.Properties.Appearance.BackColor = System.Drawing.Color.White;
+            this.txtDateModified.Properties.Appearance.Options.UseBackColor = true;
+            this.txtDateModified.Properties.DisplayFormat.FormatString = "dd/MM/yyyy";
+            this.txtDateModified.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
+            this.txtDateModified.Properties.ReadOnly = true;
+            this.txtDateModified.Size = new System.Drawing.Size(240, 22);
+            this.txtDateModified.TabIndex = 11;
+            // 
+            // txtDateCreated
+            // 
+            this.txtDateCreated.Location = new System.Drawing.Point(141, 207);
+            this.txtDateCreated.Margin = new System.Windows.Forms.Padding(10, 10, 10, 10);
+            this.txtDateCreated.Name = "txtDateCreated";
+            this.txtDateCreated.Properties.Appearance.BackColor = System.Drawing.Color.White;
+            this.txtDateCreated.Properties.Appearance.Options.UseBackColor = true;
+            this.txtDateCreated.Properties.DisplayFormat.FormatString = "dd/MM/yyyy";
+            this.txtDateCreated.Properties.DisplayFormat.FormatType = DevExpress.Utils.FormatType.DateTime;
+            this.txtDateCreated.Properties.ReadOnly = true;
+            this.txtDateCreated.Size = new System.Drawing.Size(240, 22);
+            this.txtDateCreated.TabIndex = 12;
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(33, 296);
+            this.labelControl2.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(54, 16);
+            this.labelControl2.TabIndex = 13;
+            this.labelControl2.Text = "Contains:";
+            this.labelControl2.Click += new System.EventHandler(this.labelControl2_Click);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(141, 293);
+            this.textEdit1.Margin = new System.Windows.Forms.Padding(12, 12, 12, 12);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.BackColor = System.Drawing.Color.White;
+            this.textEdit1.Properties.Appearance.Options.UseBackColor = true;
+            this.textEdit1.Properties.ReadOnly = true;
+            this.textEdit1.Size = new System.Drawing.Size(240, 22);
+            this.textEdit1.TabIndex = 14;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(33, 339);
+            this.labelControl3.Margin = new System.Windows.Forms.Padding(15, 15, 15, 15);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(60, 16);
+            this.labelControl3.TabIndex = 15;
+            this.labelControl3.Text = "Attributes:";
+            // 
+            // checkEdit1
+            // 
+            this.checkEdit1.Location = new System.Drawing.Point(141, 337);
+            this.checkEdit1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkEdit1.Name = "checkEdit1";
+            this.checkEdit1.Properties.Caption = "Read-only";
+            this.checkEdit1.Size = new System.Drawing.Size(94, 21);
+            this.checkEdit1.TabIndex = 16;
+            // 
+            // FormDirectoryInfo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 402);
+            this.Controls.Add(this.panelControl1);
+            this.Name = "FormDirectoryInfo";
+            this.Text = "FormDetail";
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            this.panelControl1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.txtName.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtSize.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtPath.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDateModified.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDateCreated.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.checkEdit1.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private DevExpress.XtraEditors.TextEdit txtDateCreated;
+        private DevExpress.XtraEditors.TextEdit txtDateModified;
+        private DevExpress.XtraEditors.TextEdit txtPath;
+        private DevExpress.XtraEditors.TextEdit txtSize;
+        private DevExpress.XtraEditors.TextEdit txtName;
+        private DevExpress.XtraEditors.LabelControl labelControl4;
+        private DevExpress.XtraEditors.LabelControl labelControl7;
+        private DevExpress.XtraEditors.LabelControl labelControl8;
+        private DevExpress.XtraEditors.LabelControl labelControl5;
+        private DevExpress.XtraEditors.LabelControl labelControl6;
+        private DevExpress.XtraEditors.CheckEdit checkEdit1;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+    }
+}
---
+++ Cilent/FormDirectoryInfo.cs
@@ -0,0 +1,41 @@
+﻿using DevExpress.XtraEditors;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cilent
+{
+    public partial class FormDirectoryInfo : DevExpress.XtraEditors.XtraForm
+    {
+        public FormDirectoryInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void labelControl7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelControl6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelControl2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelControl1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
