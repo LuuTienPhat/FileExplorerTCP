04c0293dbb02778e5ed385f54de4a784628f7b55
Lưu Tiến Phát <luutienphat@gmail.com>
2021-11-06 14:18:46 +0700

    Assign folder details to text edit components
    Add functions to count all sub-folders and sub-files in directory

--- Cilent/FormDirectoryInfo.cs
+++ Cilent/FormDirectoryInfo.cs
@@ -8,34 +8,117 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SharedClass;
+using System.IO;
 
 namespace Cilent
 {
     public partial class FormDirectoryInfo : DevExpress.XtraEditors.XtraForm
     {
-        public FormDirectoryInfo()
+
+
+        public FormDirectoryInfo(DirectoryView directoryView)
         {
             InitializeComponent();
+
+            this.Text = directoryView.directoryInfo.Name + " Properties";
+            txtName.Text = directoryView.directoryInfo.Name;
+            txtPath.Text = directoryView.directoryInfo.FullName;
+            txtDateModified.Text = directoryView.directoryInfo.LastWriteTime.ToString();
+            txtDateCreated.Text = directoryView.directoryInfo.CreationTime.ToString();
+            txtContain.Text = countFiles(directoryView).ToString() + " Files, " + countFolders(directoryView).ToString() + " Folders";
+
+            if (directoryView.directoryInfo.Attributes.HasFlag(FileAttributes.ReadOnly)) ckReadOnly.CheckState = CheckState.Checked;
+            if (directoryView.directoryInfo.Attributes.HasFlag(FileAttributes.Hidden)) ckHidden.CheckState = CheckState.Checked;
         }
 
-        private void labelControl7_Click(object sender, EventArgs e)
+        public int countFolderRecursive(DirectoryView currentDirectory, int numberOfFolders)
         {
-
+            if (currentDirectory.subDirectories.Count() == 0) return numberOfFolders;
+            else
+            {
+                numberOfFolders += currentDirectory.subDirectories.Count();
+                foreach (DirectoryView subDirectory in currentDirectory.subDirectories)
+                {
+                    numberOfFolders = countFolderRecursive(subDirectory, numberOfFolders);
+                }
+            }
+            return numberOfFolders;
         }
 
-        private void labelControl6_Click(object sender, EventArgs e)
+        private int countFolders(DirectoryView directoryView)
         {
+            int numberOfFolders = 0;
+            numberOfFolders += directoryView.subDirectories.Count();
 
+            foreach (DirectoryView subDirectory in directoryView.subDirectories)
+            {
+                numberOfFolders = countFolderRecursive(subDirectory, numberOfFolders);
+            }
+
+            return numberOfFolders;
         }
 
-        private void labelControl2_Click(object sender, EventArgs e)
+        private int countFilesRecursive(DirectoryView currentDirectory, int numberOfFiles)
         {
-
+            if (currentDirectory.subFiles.Count() == 0) return numberOfFiles;
+            else
+            {
+                numberOfFiles += currentDirectory.subFiles.Count();
+                foreach (DirectoryView subDirectory in currentDirectory.subDirectories)
+                {
+                    numberOfFiles = countFilesRecursive(subDirectory, numberOfFiles);
+                }
+            }
+            return numberOfFiles;
         }
 
-        private void labelControl1_Click(object sender, EventArgs e)
+        private int countFiles(DirectoryView directoryView)
         {
+            int numberOfFiles = 0;
+            numberOfFiles += directoryView.subFiles.Count();
 
+            foreach (DirectoryView subDirectory in directoryView.subDirectories)
+            {
+                numberOfFiles = countFilesRecursive(subDirectory, numberOfFiles);
+            }
+
+            return numberOfFiles;
         }
+
+        //private int countSizeRecursive(DirectoryView currentDirectory, int totalSize)
+        //{
+        //    if (currentDirectory.subFiles.Count() == 0) return totalSize;
+        //    else
+        //    {
+        //        totalSize += currentDirectory.subFiles.Count();
+        //        foreach (DirectoryView subDirectory in currentDirectory.subDirectories)
+        //        {
+        //            totalSize = countSizeRecursive(subDirectory, totalSize);
+        //        }
+        //    }
+        //    return totalSize;
+        //}
+
+        //private int countSize(DirectoryView directoryView)
+        //{
+        //    long totalSize = 0;
+
+        //    if(directoryView.subFiles.Count() != 0)
+        //    {
+        //        foreach (FileView subFile in directoryView.subFiles)
+        //        {
+        //            totalSize += subFile.fileInfo.Length;
+        //        }
+        //    }
+        //    totalSize += directoryView.subFiles.Count();
+
+        //    foreach (DirectoryView subDirectory in directoryView.subDirectories)
+        //    {
+        //        totalSize = countSizeRecursive(subDirectory, totalSize);
+        //    }
+
+        //    return totalSize;
+        //}
     }
 }
